09ef2e903d576f08ed048967483c8b6c7aeb200b
lovelays <karmazinpa@mail.ru>
2022-06-09 20:39:57 +0300

    the final hamster

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-
 void go_to_mono(unsigned char* image, unsigned char* data, int sizeW, int sizeH, int n) {
     int size = sizeW * sizeH * n;
     int k = 0;
@@ -22,35 +21,37 @@ void go_to_mono(unsigned char* image, unsigned char* data, int sizeW, int sizeH,
 void high_contrast(unsigned char* data, int sizeW, int sizeH, int black, int white) {
     for (int i = 2; i < sizeH-1; i++) {
         for (int j = 2; j < sizeW-1; j++) {
-            if (data[sizeW*i+j] < black)
+            if (data[sizeW*i+j] <= black-1)
                 data[sizeW*i+j] = 0;
-            if (data[sizeW*i+j] > white)
+            if (data[sizeW*i+j] >= white+1)
                 data[sizeW*i+j] = 255;
         }
     }
 }
 
-void dfs(int i, int j, int w, int h, unsigned char* data, int* v, int num){
-    v[w*i+j] = num;
-    if((i>=1) && (i<=h-1) && (j-2>=1) && (j-2<=w-1))
-        if((abs(data[w*i+j]-data[w*i+(j-2)]) <= 60) && (v[w*i+(j-2)] == 0))
-            dfs(i, j-2, w, h, data, v, num);
-
-    if((i-2>=1) && (i-2<=h-1) && (j+1>=1) && (j+1<=w-1))
-        if((abs(data[w*i+j]-data[w*(i-2)+(j+1)]) <= 60) && (v[w*(i-2)+(j+1)] == 0))
-            dfs(i-2, j+1, w, h, data, v, num);
-
-
-    if((i+2>=1) && (i+2<=h-1) && (j+1>=1) && (j+1<=w-1))
-        if((abs(data[w*i+j]-data[w*(i+2)+(j+1)]) <= 60) && (v[w*(i+2)+(j+1)] == 0))
-            dfs(i+2, j+1, w, h, data, v, num);
+void dfs(int i, int j, int w, int h, unsigned char* data, int* v, int m){
+    v[w*i+j] = m;
+    if((i>=1) && (i<=h-1) && (j-2>=1) && (j-2<=w-1)){
+        if((abs(data[w*i+j]-data[w*i+(j-2)]) <= 60)&&(v[w*i+(j-2)] == 0)){
+            dfs(i, j-2, w, h, data, v, m);
+        }
+    }
+    if((i-2>=1) && (i-2<=h-1) && (j+1>=1) && (j+1<=w-1)){
+        if((abs(data[w*i+j]-data[w*(i-2)+(j+1)])<=60)&&(v[w*(i-2)+(j+1)]==0)){
+            dfs(i-2, j+1, w, h, data, v, m);
+        }
+    }
+    if((i+2>=1) && (i+2<=h-1) && (j+1>=1) && (j+1<=w-1)){
+        if((abs(data[w*i+j]-data[w*(i+2)+(j+1)])<=60)&&(v[w*(i+2)+(j+1)]==0)){
+            dfs(i+2, j+1, w, h, data, v, m);
+        }
+    }
 }
-
 
 int main() {
     char* inputPath = "hampster.png";
     int iw, ih, n;
-    int i=0, j=0, k=0, num=0;
+    int i=0, j=0, k=0, m=0;
     unsigned char *input_image = stbi_load(inputPath, &iw, &ih, &n, 0);
     if (input_image == NULL) {
         printf("ERROR: can't read file %s\n", inputPath);
@@ -59,21 +60,22 @@ int main() {
 
     unsigned char* data = (unsigned char*)malloc(ih*iw*sizeof(unsigned char));
     int* vert = (int*) malloc(iw*ih*sizeof(int));
-    if (data == NULL || vert == NULL) {
-            printf("Memory allocation error at main()" );
-            return 0;
-        }
+    if (data == NULL || vert == NULL){
+        printf("Memory allocation error at main()" );
+        return 0;
+    }
     go_to_mono(input_image, data, iw, ih, n);
     int black = 100;
     int white = 150;
     high_contrast(data, iw, ih, black, white);
-    for(i=0;i<iw*ih;i++)
+    for(i=0;i<iw*ih;i++){
         vert[i] = 0;
+    }
     for (i=1;i<=ih-1;i++){
         for (j=1;j<=iw-1;j++){
             if(vert[iw*i+j]==0){
-                num++;
-                dfs(i, j, iw, ih, data, vert, num);
+                m+=1;
+                dfs(i, j, iw, ih, data, vert, m);
             }
         }
     }
@@ -87,8 +89,6 @@ int main() {
         odata[i+3] = 255;
         k++;
     }
-
-
     char* outputPath = "output.png";
     stbi_write_png(outputPath, iw, ih, n, odata, 0);
     free(data);
